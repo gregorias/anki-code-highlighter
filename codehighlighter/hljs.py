ad34e41b42ba78a48a05e028a22d385ada51a650
Grzegorz Milka <grzegorzmilka@gmail.com>
2023-02-18 15:00:23 +0100

    feat(hljs): add XML aliases
    Fixes #39.

--- codehighlighter/hljs.py
+++ codehighlighter/hljs.py
@@ -17,4 +17,15 @@ def get_available_languages(lang_files: List[str]) -> List[str]:
         f.removeprefix(LANG_PREFIX).removesuffix(LANG_SUFFIX)
         for f in lang_files
         if f.startswith(LANG_PREFIX) and f.endswith(LANG_SUFFIX)
-    ]
+    ] + [
+        "html",
+        "xhtml",
+        "rss",
+        "atom",
+        "xjb",
+        "xsd",
+        "xsl",
+        "plist",
+        "wsf",
+        "svg",
+    ]  # These are handled by xml.min.js.
