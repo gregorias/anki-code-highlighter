e8799e056103d85b4fe39b8d9eed1950afdbc124
Grzegorz Milka <grzegorzmilka@gmail.com>
2025-08-15 12:46:01 +0200

    refactor: fix lints

--- codehighlighter/assets.py
+++ codehighlighter/assets.py
@@ -10,7 +10,8 @@ import os.path
 import pathlib
 import re
 import typing
-from typing import Callable, List, Optional, Protocol, Tuple
+from collections.abc import Callable
+from typing import Protocol
 
 from anki.media import MediaManager
 
@@ -42,9 +43,9 @@ def has_newer_version(media: MediaManager, version_asset: str) -> bool:
     """
     Returns whether the plugin has newer asset version.
 
-    :param media MediaManager Anki's media manager
-    :param version_asset str: The version asset filename.
-    :rtype bool Whether the plugin has newer asset version.
+    :param media Anki's media manager
+    :param version_asset The version asset filename.
+    :rtype Whether the plugin has newer asset version.
     """
     new_version = read_asset_version(assets_directory() / version_asset)
     old_version = read_asset_version(anki_media_directory(media) / version_asset)
@@ -56,7 +57,7 @@ def has_newer_version(media: MediaManager, version_asset: str) -> bool:
         return False
 
 
-def read_asset_version(asset_version_path: pathlib.Path) -> Optional[int]:
+def read_asset_version(asset_version_path: pathlib.Path) -> int | None:
     """Reads the integer representing the asset version from the file."""
     try:
         with open(asset_version_path, "r") as f:
@@ -72,22 +73,21 @@ class AnkiAssetManager:
         modify_templates: Callable[[Callable[[str], str]], None],
         media: MediaManager,
         asset_prefix: str,
-        css_assets: List[str],
-        js_assets: List[str],
+        css_assets: list[str],
+        js_assets: list[str],
         guard: str,
         class_name: str,
     ):
         """
-        :param modify_templates Callable[[Callable[[str], str]],
-                                                          None]:
+        :param modify_templates
             A function that can modify card templates.
-        :param media anki.media.MediaManager: The active Anki media manager.
-        :param asset_prefix str: The prefix used for this plugin's assets.
-        :param css_assets List[str]: All CSS files used by this plugin.
-        :param js_assets List[str]: All JS files to be imported by this plugin.
-        :param guard str A guard string used for HTML comments wrapping the imports.
-        :param class_name str: The unique HTML class name that this manager can
-            use to identify its HTML elements.
+        :param media The active Anki media manager.
+        :param asset_prefix The prefix used for this plugin's assets.
+        :param css_assets All CSS files used by this plugin.
+        :param js_assets All JS files to be imported by this plugin.
+        :param guard A guard string used for HTML comments wrapping the imports.
+        :param class_name The unique HTML class name that this manager can use
+            to identify its HTML elements.
         """
         self.modify_templates = modify_templates
         self.media = media
@@ -111,9 +111,7 @@ class AnkiAssetManager:
 
     def delete_assets(self) -> None:
         self.modify_templates(
-            lambda tmpl: delete_import_statements(
-                guard=self.guard, class_name=self.class_name, tmpl=tmpl
-            )
+            lambda tmpl: delete_import_statements(guard=self.guard, tmpl=tmpl)
         )
         delete_media_assets(self.asset_prefix, self.media)
 
@@ -137,11 +135,11 @@ def anki_media_directory(media: MediaManager) -> pathlib.Path:
     return pathlib.Path(media.dir())
 
 
-def list_files_with_prefix(dir: pathlib.Path, asset_prefix: str) -> List[str]:
+def list_files_with_prefix(dir: pathlib.Path, asset_prefix: str) -> list[str]:
     return [f for f in os.listdir(dir) if f.startswith(asset_prefix)]
 
 
-def list_plugin_media_files(media: MediaManager, plugin_asset_prefix: str) -> List[str]:
+def list_plugin_media_files(media: MediaManager, plugin_asset_prefix: str) -> list[str]:
     """
     Return's the plugin's media files.
 
@@ -150,7 +148,7 @@ def list_plugin_media_files(media: MediaManager, plugin_asset_prefix: str) -> Li
     :param media MediaManager
     :param plugin_asset_prefix str: The plugin's identifying prefix, e.g.,
       ('_ch').
-    :rtype List[str]: The plugin's media files.
+    :rtype list[str]: The plugin's media files.
     """
     return list_files_with_prefix(anki_media_directory(media), plugin_asset_prefix)
 
@@ -168,28 +166,27 @@ def delete_media_assets(asset_prefix: str, media: MediaManager) -> None:
     media.trash_files(my_assets)
 
 
-def guards(guard: str) -> Tuple[str, str]:
+def guards(guard: str) -> tuple[str, str]:
     """
     Creates HTML comments bracketing import statements.
 
     :param guard str A guard string used for HTML comments wrapping the imports.
-    :rtype Tuple[str, str]
     """
     return (f"<!-- {guard} BEGIN -->\n", f"<!-- {guard} END -->\n")
 
 
 def append_import_statements(
-    css_assets: List[str], js_assets: List[str], guard: str, class_name: str, tmpl: str
+    css_assets: list[str], js_assets: list[str], guard: str, class_name: str, tmpl: str
 ) -> str:
     """
     Appends import statements to a card template.
 
-    :param css_assets List[str]
-    :param js_assets List[str]
-    :param guard str A guard string used for HTML comments wrapping the imports.
-    :param class_name str A class name that identifies this plugin.
-    :param tmpl str
-    :rtype str: A template with added import statements.
+    :param css_assets
+    :param js_assets
+    :param guard A guard string used for HTML comments wrapping the imports.
+    :param class_name A class name that identifies this plugin.
+    :param tmpl
+    :rtype A template with added import statements.
     """
     IMPORT_STATEMENTS = "".join(
         [
@@ -209,14 +206,13 @@ def append_import_statements(
     return tmpl + gap + GUARD_BEGIN + IMPORT_STATEMENTS + GUARD_END
 
 
-def delete_import_statements(guard: str, class_name: str, tmpl: str) -> str:
+def delete_import_statements(guard: str, tmpl: str) -> str:
     """
     Deletes import statements from a card template.
 
-    :param guard str A guard string used for HTML comments wrapping the imports.
-    :param class_name str A class name that identifies this plugin.
-    :param tmpl str
-    :rtype str: A template with deleted import statements.
+    :param guard A guard string used for HTML comments wrapping the imports.
+    :param tmpl
+    :rtype template with deleted import statements.
     """
     GUARD_BEGIN, GUARD_END = guards(guard)
     return re.sub(
--- test/test_assets.py
+++ test/test_assets.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 import tempfile
-from textwrap import dedent
 import unittest
+from textwrap import dedent
 
 from codehighlighter import assets
 from codehighlighter.assets import append_import_statements, delete_import_statements
@@ -37,7 +36,7 @@ class AssetsTestCase(unittest.TestCase):
 
     def test_read_asset_version_returns_version(self):
         with tempfile.NamedTemporaryFile() as version_f:
-            version_f.writelines([b'42'])
+            version_f.writelines([b"42"])
             version_f.flush()
             self.assertEqual(assets.read_asset_version(version_f.name), 42)
 
@@ -51,52 +50,70 @@ class AssetsTestCase(unittest.TestCase):
                     <h4>Notes</h4>
                     {{Notes}}"""
 
-        GUARD = 'PLUGIN (Addon 123)'
-        CLASS_NAME = 'anki-ch'
+        GUARD = "PLUGIN (Addon 123)"
+        CLASS_NAME = "anki-ch"
 
-        new_tmpl = append_import_statements(['c.css'], ['j.js'], GUARD,
-                                            CLASS_NAME, tmpl)
-        self.assertEqual(delete_import_statements(GUARD, CLASS_NAME, new_tmpl),
-                         tmpl + '\n')
+        new_tmpl = append_import_statements(
+            ["c.css"], ["j.js"], GUARD, CLASS_NAME, tmpl
+        )
+        self.assertEqual(delete_import_statements(GUARD, new_tmpl), tmpl + "\n")
 
     def test_append_import_statements_adds_them_with_a_gap(self):
         self.assertEqual(
-            append_import_statements(['c.css'], ['j.js'],
-                                     'Anki Code Highlighter (Addon 112228974)',
-                                     'plugin', '{{Cloze}}'),
-            dedent('''\
+            append_import_statements(
+                ["c.css"],
+                ["j.js"],
+                "Anki Code Highlighter (Addon 112228974)",
+                "plugin",
+                "{{Cloze}}",
+            ),
+            dedent(
+                """\
             {{Cloze}}
 
             <!-- Anki Code Highlighter (Addon 112228974) BEGIN -->
             <link rel="stylesheet" href="c.css" class="plugin">
             <script src="j.js" class="plugin"></script>
             <!-- Anki Code Highlighter (Addon 112228974) END -->
-            '''))
+            """
+            ),
+        )
 
     def test_append_import_statements_adds_them_with_a_gap_and_minds_a_newline_in_template(
-            self):
+        self,
+    ):
         self.assertEqual(
-            append_import_statements(['c.css'], ['j.js'],
-                                     'Anki Code Highlighter (Addon 112228974)',
-                                     'plugin', '{{Cloze}}\n'),
-            dedent('''\
+            append_import_statements(
+                ["c.css"],
+                ["j.js"],
+                "Anki Code Highlighter (Addon 112228974)",
+                "plugin",
+                "{{Cloze}}\n",
+            ),
+            dedent(
+                """\
             {{Cloze}}
 
             <!-- Anki Code Highlighter (Addon 112228974) BEGIN -->
             <link rel="stylesheet" href="c.css" class="plugin">
             <script src="j.js" class="plugin"></script>
             <!-- Anki Code Highlighter (Addon 112228974) END -->
-            '''))
+            """
+            ),
+        )
 
     def test_delete_import_statements_deletes_new_style_imports(self):
-        TMPL = dedent('''\
+        TMPL = dedent(
+            """\
             {{Cloze}}
 
             <!-- Anki Code Highlighter (Addon 112228974) BEGIN -->
             <link rel="stylesheet" href="c.css" class="plugin">
             <script src="j.js" class="plugin"></script>
             <!-- Anki Code Highlighter (Addon 112228974) END -->
-            ''')
+            """
+        )
         self.assertEqual(
-            delete_import_statements('Anki Code Highlighter (Addon 112228974)',
-                                     'plugin', TMPL), '{{Cloze}}\n')
+            delete_import_statements("Anki Code Highlighter (Addon 112228974)", TMPL),
+            "{{Cloze}}\n",
+        )
