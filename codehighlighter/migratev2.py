b3dd61f9f72535e5ac24731ae3f77f833ff40b97
Grzegorz Milka <grzegorzmilka@gmail.com>
2025-09-22 07:42:48 +0200

    feat: add get_hljs_notes

--- codehighlighter/migratev2.py
+++ codehighlighter/migratev2.py
@@ -1,10 +1,23 @@
 """The module for migrating cards to ACH v2."""
 
+from collections.abc import Iterable
+
+from anki.collection import Collection
+from anki.notes import Note
+
 from .pygments_highlighter import LexerName, get_lexer_name_by_alias
 
 __all__ = [
     "hljs_to_pygments_lang",
 ]
+
+
+def get_hljs_notes(col: Collection) -> Iterable[Note]:
+    """Gets Highlight.js-like notes."""
+    # Highlight.js only uses <pre><code> blocks.
+    # The code block has a "language-foo" as a class.
+    note_ids = col.find_notes("pre language-")
+    return (col.get_note(note_id) for note_id in note_ids)
 
 
 def hljs_to_pygments_lang(hljs_lang: str) -> LexerName | None:
