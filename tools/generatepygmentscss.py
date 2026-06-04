de4f8ec9d472ea7b61541b41c3dadc93675ac424
Grzegorz Milka <grzegorzmilka@gmail.com>
2026-06-04 09:04:34 +0200

    refactor: add docs for Pygments CSS generation

--- tools/generatepygmentscss.py
+++ tools/generatepygmentscss.py
@@ -1,4 +1,10 @@
-"""Generates the CSS for pygments highlighting."""
+"""Generates the CSS for Pygments highlighting."""
+
+# The style sheet consists of 2 sections:
+#
+# 1. Preamble, which describes the surounding box and background.
+# 2. Tokens, which describes the individual tokens. The meaning of a token is
+#    consistent with Pygments.
 
 import textwrap
 
@@ -16,39 +22,46 @@ SOLARIZED_LIGHT_BORDER_COLOR = "#cdbc84"
 SOLARIZED_DARK_BORDER_COLOR = "#052831"
 PYGMENTS_CLASS = "gch-pygments"
 NIGHT_MODE_CLASS = "night_mode"
-DAY_MODE_SELECTOR = cssutils.css.Selector(f".{PYGMENTS_CLASS}")
-NIGHT_MODE_SELECTOR = cssutils.css.Selector(f".{NIGHT_MODE_CLASS} .{PYGMENTS_CLASS}")
+DAY_MODE_SELECTOR_STR = f".{PYGMENTS_CLASS}"
+NIGHT_MODE_SELECTOR_STR = f".{NIGHT_MODE_CLASS} .{PYGMENTS_CLASS}"
 
 
 def delete_pygments_css_preamble(
-    pygments_css_rules: cssutils.css.CSSRuleList, selector: cssutils.css.Selector
-):
+    pygments_css_rules: cssutils.css.CSSRuleList, prefix_selector: str
+) -> None:
     """Deletes code block styles.
 
     This plugin defines its own style for code blocks, so we don't want to keep
     the default Pygments styles.
+
+    Arguments:
+        pygments_css_rules: The list of CSS rules to delete the preamble from.
+        prefix_selector: The CSS selector that prefixes all Pygments rules.
     """
     while PYGMENTS_CLASS not in pygments_css_rules[0].selectorText:
         pygments_css_rules.pop(0)
 
     for rule in pygments_css_rules:
-        if rule.selectorText == selector.selectorText:
+        if rule.selectorText == prefix_selector:
             pygments_css_rules.remove(rule)
             break
 
 
-def get_pygments_css(
-    style: pygments.style.Style, selector: cssutils.css.Selector
+# Using str for the selector, because that's how Pygments likes it.
+def get_pygments_token_css(
+    style: pygments.style.Style, prefix_selector: str
 ) -> cssutils.css.CSSStyleSheet:
-    """Gets the CSS of a particular Pygments style without the preamble."""
+    """Gets the CSS of a particular Pygments style without the preamble.
+
+    Arguments:
+      prefix_selector: The CSS selector to prefix all Pygments rules with.
+    """
     # In the context of this plugin, only the HTML formatter is used.
     html_formatter: pygments.formatters.html.HtmlFormatter = (
         pygments.formatters.get_formatter_by_name("html", style=style)
     )
-    pygments_css = cssutils.parseString(
-        html_formatter.get_style_defs(selector.selectorText)
-    )
-    delete_pygments_css_preamble(pygments_css.cssRules, selector)
+    pygments_css = cssutils.parseString(html_formatter.get_style_defs(prefix_selector))
+    delete_pygments_css_preamble(pygments_css.cssRules, prefix_selector)
     return pygments_css
 
 
@@ -56,8 +69,7 @@ def generate_highlighter_pygments_css_preamble(
     day_style: pygments.style.Style,
     night_style: pygments.style.Style,
 ) -> cssutils.css.CSSStyleSheet:
-    preamble = textwrap.dedent(
-        f"""
+    preamble = textwrap.dedent(f"""
         .{PYGMENTS_CLASS}>pre {{
           background: {day_style.background_color};
           color: {day_style.styles[pygments.style.Token]};
@@ -72,8 +84,7 @@ def generate_highlighter_pygments_css_preamble(
           color: {night_style.styles[pygments.style.Token]};
           border-color: {SOLARIZED_DARK_BORDER_COLOR};
         }}
-    """
-    ).strip()
+    """).strip()
     return cssutils.parseString(preamble)
 
 
@@ -83,8 +94,12 @@ def generate_highlighter_pygments_css() -> cssutils.css.CSSStyleSheet:
     night_style = pygments.styles.get_style_by_name(NIGHT_STYLE)
 
     css = generate_highlighter_pygments_css_preamble(day_style, night_style)
-    css.cssRules.extend(get_pygments_css(day_style, DAY_MODE_SELECTOR).cssRules)
-    css.cssRules.extend(get_pygments_css(night_style, NIGHT_MODE_SELECTOR).cssRules)
+    css.cssRules.extend(
+        get_pygments_token_css(day_style, DAY_MODE_SELECTOR_STR).cssRules
+    )
+    css.cssRules.extend(
+        get_pygments_token_css(night_style, NIGHT_MODE_SELECTOR_STR).cssRules
+    )
     return css
 
 
